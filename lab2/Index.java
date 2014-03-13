c549d611191374af06da4e8f5b8847b5cecc67c5
Andrew Penniman <awpennim@student.umass.edu>
2014-03-13 15:39:56 -0400

    checkpoint before unzipping lab2_material.zip

--- lab2/Index.java
+++ lab2/Index.java
@@ -4,9 +4,33 @@ public class Index{
 	public static void main(String[] args){
 		int n = Integer.parseInt(args[0]);
 		
-		ArrayList<String> files = new ArrayList<String>();
+		ArrayList<Mapper> files = new ArrayList<Mapper>();
 		for(int i = 0; i < args.length; i++){
-			files.add(args[i]);
+			files.add(new Mapper(args[i]));
 		}
+		
+		for(int i = 0; i < files.size(); i++){
+			files.get(i).start();	
+		}
+	}
+}
+
+class Mapper extends Thread{
+	private String filename;
+	
+	public Mapper(String filename){
+		this.filename = filename;	
+	}
+	
+	public void run(){
+		File file = new File(filename);
+		
+		BufferedReader fileContents = BufferedReader(new FileReader(file))
+		
+		while(fileContents.hasNextLine()){
+			String line = fileContents.readLine();
+			
+			System.out.println(line);
+		}	
 	}
 }
