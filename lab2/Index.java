f58e1062f0699b454d886969e382d2cbf0cbd7d5
Andrew Penniman <awpennim@student.umass.edu>
2014-03-13 15:28:33 -0400

    it can take the input and put it into correct data structures

--- lab2/Index.java
+++ lab2/Index.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class Index{
 	public static void main(String[] args){
-		System.out.println("hey");	
+		int n = Integer.parseInt(args[0]);
+		
+		ArrayList<String> files = new ArrayList<String>();
+		for(int i = 0; i < args.length; i++){
+			files.add(args[i]);
+		}
 	}
 }
